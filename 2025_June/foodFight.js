d4ef218008493eb470193a044db805d799609f82
윤지원 <dusqha6731@naver.com>
2025-06-27 23:45:18 +0900

    [Algorithm] update:푸드 파이트  반복문 줄인 버전 추가

--- 2025_June/foodFight.js
+++ 2025_June/foodFight.js
@@ -4,19 +4,29 @@
  * 
  */
 
+// function solution(food) {
+//   let list = '';
+//   for (let i = 1; i <= food.length; i++){
+//     const count = Math.floor(food[i]/2);
+//     list += i.toString().repeat(count);
+//   }
+//   list += '0';
+//   for (let i = food.length - 1; i >= 1; i--){
+//     const count = Math.floor(food[i]/2);
+//     list += i.toString().repeat(count);
+//   }
+//   return list;
+// }
+
+solution([1, 3, 4, 6]);
+
+// 반복문 줄인 버전전
 function solution(food) {
   let list = '';
   for (let i = 1; i <= food.length; i++){
     const count = Math.floor(food[i]/2);
     list += i.toString().repeat(count);
   }
-  list += '0';
-  for (let i = food.length - 1; i >= 1; i--){
-    const count = Math.floor(food[i]/2);
-    list += i.toString().repeat(count);
-  }
-  return list;
+  // 0을 붙인뒤엔 
+  return list + '0' + list.split('').reverse().join('');
 }
-
-solution([1, 3, 4, 6]);
-
