1beda9c6a84309e08f16d58a8f5c46e49a153380
윤지원 <dusqha6731@naver.com>
2025-07-19 23:26:45 +0900

    [Algorithm] solve: 행렬의 덧셈 해결

---
+++ 2025_July/addMatrices.js
@@ -0,0 +1,12 @@
+// 프로그래머스 - 행렬의 덧셈
+
+function solution(arr1, arr2) {
+  let arr = [[]];
+  for (let i=0; i < arr1.length; i++) {
+    arr[i] = [];
+    for (let j=0; j < arr1[0].length; j++) {
+      arr[i][j] =  arr1[i][j] + arr2[i][j]
+    }
+  }
+  return arr;
+}
