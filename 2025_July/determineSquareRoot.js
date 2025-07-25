6632e13137e77205b39dad5c5bd7e03c783bc81f
윤지원 <dusqha6731@naver.com>
2025-07-25 13:59:45 +0900

    [Algorithm] solve: 정수 제곱근 판별 해결

---
+++ 2025_July/determineSquareRoot.js
@@ -0,0 +1,14 @@
+// 프로그래머스 - 정수 제곱근 판별
+
+function solution(n) {
+  // n의 제곱근
+  let sqrt = Math.sqrt(n);
+  
+  // 제곱근이 정수인지 확인
+  if (sqrt === Math.floor(sqrt)) {
+    // 완전제곱수라면 (sqrt + 1)의 제곱 반환
+    return (sqrt + 1) * (sqrt + 1);
+  } else {
+    return -1;
+  }
+}
