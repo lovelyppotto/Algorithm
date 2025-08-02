f6b6608b1eae7e1af9fc08298b9862d3597c0fc4
윤지원 <dusqha6731@naver.com>
2025-08-03 00:00:31 +0900

    [Algorithm] solve: 최대공약수와 최소공배수

--- 2025_August/whatGcd.js
+++ 2025_August/whatGcd.js
@@ -1,17 +1,19 @@
+// 프로그래머스 - 최대공약수와 최소공배수
+
 function solution(n, m) {
-    function gcd(a, b) {
-        if (b === 0) {
-            return a;
-        }
-        return gcd(b, a % b);
+  function gcd(a, b) {
+    if (b === 0) {
+      return a;
     }
-    
-    function lcm(a, b) {
-        return (a * b) / gcd(a, b);
-    }
-    
-    const whatGcd = gcd(n, m);
-    const whatLcm = lcm(n, m);
-    
-    return [whatGcd, whatLcm];
+    return gcd(b, a % b);
+  }
+  
+  function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+  }
+  
+  const whatGcd = gcd(n, m);
+  const whatLcm = lcm(n, m);
+  
+  return [whatGcd, whatLcm];
 }
