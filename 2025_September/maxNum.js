6ffa5ba00b3486ea5d091a47499222757ccf14ed
윤지원 <dusqha6731@naver.com>
2025-09-15 15:56:32 +0900

    [Algorithm] update: 배열 수 0일때 조건 추가

--- 2025_September/maxNum.js
+++ 2025_September/maxNum.js
@@ -9,8 +9,10 @@ function solution(numbers) {
     if ( a+b < b+a ) return 1;
     return 0;
   });
-  return strNums.join('')
+  // 배열의 수가 모두 0일 경우 고려
+  return strNums[0] === '0' ? '0' : strNums.join('');
 }
 
 console.log(solution([6, 10, 2]))
+console.log(solution([0, 0, 0]))
 console.log(solution([1, 10, 100, 1000]))
