6380f3918eeeb8e34d206b03742fcb553bd8bde0
윤지원 <dusqha6731@naver.com>
2025-09-15 15:53:37 +0900

    [Algorithm] solve: 가장 큰 수 해결

---
+++ 2025_September/maxNum.js
@@ -0,0 +1,16 @@
+// 프로그래머스 - 가장 큰 수 
+
+function solution(numbers) {
+  // 비교 함수가 문자열 +로 동작하도록 형변환
+  let strNums = numbers.map(String);
+
+  strNums.sort((a, b) => {
+    if ( a+b > b+a ) return -1;
+    if ( a+b < b+a ) return 1;
+    return 0;
+  });
+  return strNums.join('')
+}
+
+console.log(solution([6, 10, 2]))
+console.log(solution([1, 10, 100, 1000]))
