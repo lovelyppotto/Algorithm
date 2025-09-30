a44e47192a7293d3cf4cbd67a8147596a58e8b02
윤지원 <dusqha6731@naver.com>
2025-09-30 23:44:33 +0900

    [Algorithm] solve: 3진법 뒤집기 해결

---
+++ 2025_September/ternaryReversal.js
@@ -0,0 +1,8 @@
+function solution(n) {
+  // 3진법으로 변환
+  const ternary = n.toString(3);
+  // 문자열 뒤집기
+  const reversed = ternary.split('').reverse().join('');
+  // 10진법으로 변환
+  return parseInt(reversed, 3);
+}
